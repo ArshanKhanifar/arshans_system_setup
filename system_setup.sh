440b73b0e0bef6f7944be712951b5cd7d9bef180
Arshan Khanifar <arshan@Amirs-MBP.lan>
2020-01-17 15:31:19 -0500

    merging system setup and vim setup

--- system_setup.sh
+++ system_setup.sh
@@ -1,1 +1,26 @@
-sh ./vim_setup.sh
+# fail early
+set -eax
+
+USER_NAME="arshankhanifar"
+REPO_NAME="arshans_system_setup"
+NOPLUGINS_VIMRC="no_plugins.vimrc"
+VUNDLE_PLUGINS="vundle_plugins.vim"
+
+# install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# clone this repo:
+cd ~
+git clone https://github.com/$USER_NAME/$REPO_NAME
+
+# Add shortcuts
+echo "source ~/${REPO_NAME}/${NOPLUGINS_VIMRC}" >> ~/.vimrc
+
+# install vundle:
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install vundle plugins:
+echo "source ~/${REPO_NAME}/${VUNDLE_PLUGINS}" >> ~/.vimrc
+vim +PluginInstall +qall
+
+
