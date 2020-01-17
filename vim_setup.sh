5c6ff52802f84ac1cb015367e7f3044ae94b4634
Arshan Khanifar <arshankhanifar@gmail.com>
2020-01-17 15:19:48 -0500

    Update vim_setup.sh
    fix nit

--- vim_setup.sh
+++ vim_setup.sh
@@ -11,13 +11,13 @@ cd ~
 git clone https://github.com/$USER_NAME/$REPO_NAME
 
 # Add shortcuts
-echo "source ~/${REPO_NAME}/{$NOPLUGINS_VIMRC}" >> ~/.vimrc
+echo "source ~/${REPO_NAME}/${NOPLUGINS_VIMRC}" >> ~/.vimrc
 
 # install vundle:
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install vundle plugins:
-echo "source ~/${REPO_NAME}/{$VUNDLE_PLUGINS}" >> ~/.vimrc
+echo "source ~/${REPO_NAME}/${VUNDLE_PLUGINS}" >> ~/.vimrc
 vim +PluginInstall +qall
 
 
