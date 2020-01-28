d0ba0347017c1646b77d2a7771f8962cd3926b5e
Coop-2 <coop-2@localhost.localdomain>
2020-01-28 14:33:20 -0500

    added machine specific instructions

--- system_setup.sh
+++ system_setup.sh
@@ -8,6 +8,18 @@ VUNDLE_PLUGINS="vundle_plugins.vim"
 PLUG_PLUGINS="plug.vim"
 SHELL_RC_FILE=".zshrc"
 
+MACHINE_MAC="Mac"
+MACHINE_LINUX="Linux"
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=$MACHINE_LINUX;;
+    Darwin*)    machine=$MACHINE_MAC;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
 # install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
@@ -36,12 +48,14 @@ vim +'PlugInstall --sync' +qa
 ##### password-requiring commands
 chsh -s $(which zsh)
 
-# install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ "${machine}" = "${MACHINE_MAC}" ]; then
+  # install homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  
+  # brew installations
+  brew install ctags
 
-# brew installations
-brew install ctags
-
-alias ctags="`brew --prefix`/bin/ctags"
-alias ctags >> ~/$SHELL_RC_FILE
-
+  alias ctags="`brew --prefix`/bin/ctags"
+  alias ctags >> ~/$SHELL_RC_FILE
+  
+fi
