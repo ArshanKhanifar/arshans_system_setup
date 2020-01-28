d9918e68a9a2b0a8a24743e6f9cb8b290e954219
Coop-2 <coop-2@localhost.localdomain>
2020-01-28 12:26:38 -0500

    added junegunns plug installation

--- system_setup.sh
+++ system_setup.sh
@@ -25,6 +25,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "source ~/${REPO_NAME}/${VUNDLE_PLUGINS}" >> ~/.vimrc
 vim +PluginInstall +qall
 
+# install plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # install plug plugins:
 echo "source ~/${REPO_NAME}/${PLUG_PLUGINS}" >> ~/.vimrc
 vim +'PlugInstall --sync' +qa
