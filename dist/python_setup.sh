8b6891ebae32fd547bde4253a53ad6a63fbdf5e8
arshankhanifar <arshankhanifar@gmail.com>
2024-06-30 12:45:57 -0400

    fix: add docker flag to python sc

--- dist/python_setup.sh
+++ dist/python_setup.sh
@@ -51,6 +51,7 @@ function install() {
   if ! [ $runtime = "docker" ]; then
     grep -qxF "\$nrconf{restart} = 'a'" /etc/needrestart/needrestart.conf || echo "\$nrconf{restart} = 'a'" | sudo tee -a /etc/needrestart/needrestart.conf;
   fi
+  sudo apt-get update && apt-get install -y software-properties-common
   sudo add-apt-repository -y ppa:deadsnakes/ppa
   sudo apt-get update
   sudo apt install -y python3.11
